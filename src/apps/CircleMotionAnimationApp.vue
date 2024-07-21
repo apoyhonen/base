eebb75cef13317ddebbdb368989367923a81fe26
Antti Pöyhönen <antti.poyhonen@ekahau.com>
2024-07-21 20:13:29 +0300

    circlemotion use line instead of arc, particles mouse follow toggle, reset canvas option

--- src/apps/CircleMotionAnimationApp.vue
+++ src/apps/CircleMotionAnimationApp.vue
@@ -11,6 +11,14 @@
         <p>frame: {{ frameCount }}</p>
         <br>
         <button @click="isRunning = !isRunning">START / STOP</button>
+
+        <br><br><br><br>
+
+        <span>Following: {{ particlesFollowMouse ? 'mouse' : 'middle' }}</span>
+        <br>
+        <button @click="particlesFollowMouse = !particlesFollowMouse">TOGGLE CENTER</button>
+        <br><br>
+        <button @click="clearCanvas">RESET CANVAS</button>
       </td>
 
       <td>
@@ -25,7 +33,7 @@
 
 <script setup>
 
-import { onMounted, ref, watch } from "vue";
+import { computed, onMounted, ref, watch } from "vue";
 import { angleBetweenPointsRadian, randomBetween, randomIntBetween } from "@/util/MathUtil";
 import { randomColor } from "@/util/ColorUtil";
 
@@ -44,6 +52,9 @@ onMounted(() => {
   middlePoint.x = canvas.width / 2;
   middlePoint.y = canvas.height / 2;
 
+  particleMaxDistance = Math.min(canvas.width, canvas.height) / 4;
+  particleMinDistance = particleMaxDistance / 2;
+
   initParticles();
 
   draw(); // init
@@ -56,18 +67,17 @@ watch(isRunning, () => {
 function draw() {
   frameCount.value++;
 
-  clear();
-  drawParticles();
+  // fade out old draws slowly over time, "fade to gray"
+  c.fillStyle = 'rgba(255, 255, 255, 0.08)';
+  c.fillRect(0, 0, canvas.width, canvas.height);
+
+  particles.forEach(particle => particle.update());
 
   if (isRunning.value && isAppActive()) requestAnimationFrame(draw); // redraw as soon as animation frame is available
 }
 
 function isAppActive() {
   return document.getElementById("circleMotionCanvas") !== null;
-}
-
-function clear() {
-  c.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 let middlePoint = { x: 0, y: 0 };
@@ -79,6 +89,10 @@ function canvasClicked(e) {
   const eventX = e.clientX - canvas.offsetLeft;
   const eventY = e.clientY - canvas.offsetTop;
   angleInRadians.value = angleBetweenPointsRadian(mousePoint.value.x, mousePoint.value.y, eventX, eventY);
+}
+
+function clearCanvas() {
+  c.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 // key and mouse handlers
@@ -94,8 +108,19 @@ function mouseMoveHandler(e) {
 
 // particles
 
-let particles = [];
+const particlesFollowMouse = ref(false);
+const particleCenterPoint = computed(() => {
+  if (particlesFollowMouse.value) {
+    return { x: mousePoint.value.x, y: mousePoint.value.y }
+  } else {
+    return middlePoint;
+  }
+})
 
+let particleMinDistance = 100;
+let particleMaxDistance = 200;
+
+let particles = [];
 function initParticles() {
   for (let i = 0; i < 100; i++) {
     particles.push(new Particle(middlePoint.x, middlePoint.y, 5, 'blue'));
@@ -107,33 +132,34 @@ function Particle(x, y, radius) {
   this.y = y;
   this.radius = radius;
   this.color = randomColor();
-  this.radians = randomBetween(0, Math.PI * 2);
-  this.velocity = randomBetween(0.02, 0.04);
-  this.oscillation = randomIntBetween(100, 500); // factor to oscillate past radius
+  this.radians = Math.random() * Math.PI * 2;
+  this.velocity = randomBetween(0.015, 0.03);
+  this.distanceFromCenter = randomIntBetween(particleMinDistance, particleMaxDistance); // factor to oscillate past radius
 
   this.update = () => {
     // move points over time
     this.radians += this.velocity;
     if (this.radians >= Math.PI * 2) this.radians -= Math.PI * 2;
 
-    this.x = x + Math.cos(this.radians) * this.oscillation;
-    this.y = y + Math.sin(this.radians) * this.oscillation;
+    const lastPoint = { x: this.x, y: this.y };
 
-    this.draw();
+    this.x = particleCenterPoint.value.x + Math.cos(this.radians) * this.distanceFromCenter;
+    this.y = particleCenterPoint.value.y + Math.sin(this.radians) * this.distanceFromCenter;
+
+    this.draw(lastPoint);
   }
 
-  this.draw = () => {
-    c.fillStyle = this.color;
+  this.draw = lastPoint => {
+    c.strokeStyle = this.color;
+    c.lineWidth = this.radius;
 
     c.beginPath();
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
-    c.fill();
+    c.moveTo(lastPoint.x, lastPoint.y);
+    c.lineTo(this.x, this.y);
+    c.stroke();
+
     c.closePath(); // TODO is this needed?
   }
-}
-
-function drawParticles() {
-  particles.forEach(particle => particle.update());
 }
 
 </script>
